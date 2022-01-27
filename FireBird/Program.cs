27324f78767b3c786d7dffcd3f1b9eec3e40b88e
SenerDemiral <sener.demiral@gmail.com>
2022-01-27 10:24:23 +0300

    27.01.22

--- FireBird/Program.cs
+++ FireBird/Program.cs
@@ -9,7 +9,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
-builder.Services.AddDevExpressBlazor(configure => configure.SizeMode = DevExpress.Blazor.SizeMode.Small);
+builder.Services.AddDevExpressBlazor(configure => { 
+    configure.SizeMode = DevExpress.Blazor.SizeMode.Small;
+    //configure.BootstrapVersion = DevExpress.Blazor.BootstrapVersion.v5;
+
+});
+
+//builder.Services.Configure<DevExpress.Blazor.Configuration.GlobalOptions>(options => {
+//    options.BootstrapVersion = DevExpress.Blazor.BootstrapVersion.v5;
+//});
 
 builder.Services.AddSingleton<DataAccess>();
 
